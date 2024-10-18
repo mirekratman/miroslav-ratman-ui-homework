51cc899ec1974ada89ed9486111a82b04a1389df
Mirek Ratman <mirek.ratman@gmail.com>
2024-10-18 15:41:04 +0200

    remove unnecessary js file

--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
-    return React.createElement("h1", null, "Hello, React with Rspack!");
-};
-ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
